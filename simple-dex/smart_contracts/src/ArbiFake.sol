ad58962068897d0b37b31bac1da5f23e921bc612
Lianum Frada Bahesna <fbahezna@gmail.com>
2025-10-13 19:09:30 +0700

    enhance(simple-dex): enhance smart contract faucet and update deploy into testnet

--- simple-dex/smart_contracts/src/ArbiFake.sol
+++ simple-dex/smart_contracts/src/ArbiFake.sol
@@ -4,7 +4,20 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ArbiFake is ERC20 {
+    mapping(address => bool) public claimed;
+    mapping(address => uint256) public lastClaim;
+
     constructor(uint256 supply) ERC20("ArbiFake", "AFAKE") {
         _mint(msg.sender, supply * 10 ** decimals());
+    }
+
+    function faucet() external {
+        // allow first claim OR following claims after 1 day
+        require(lastClaim[msg.sender] == 0 || block.timestamp - lastClaim[msg.sender] >= 1 days, "Claim once per day");
+        
+        lastClaim[msg.sender] = block.timestamp;
+        claimed[msg.sender] = true;
+
+        _mint(msg.sender, 10 * 10 ** decimals());
     }
 }
--- simple-dex/smart_contracts/src/DogeFake.sol
+++ simple-dex/smart_contracts/src/DogeFake.sol
@@ -4,8 +4,21 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DogeFake is ERC20 {
+    mapping(address => bool) public claimed;
+    mapping(address => uint256) public lastClaim;
+
     constructor(uint256 supply) ERC20("DogeFake", "DFAKE"){
         _mint(msg.sender, supply * 10 ** decimals());
+    }
+
+    function faucet() external {
+        // allow first claim OR following claims after 1 day
+        require(lastClaim[msg.sender] == 0 || block.timestamp - lastClaim[msg.sender] >= 1 days, "Claim once per day");
+        
+        lastClaim[msg.sender] = block.timestamp;
+        claimed[msg.sender] = true;
+
+        _mint(msg.sender, 100 * 10 ** decimals());
     }
 }
 
